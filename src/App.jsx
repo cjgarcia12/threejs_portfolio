3ab0be848a021449b55bff641a9205bc5afbf06a
cjgarcia12 <christian.jgarcia03@gmail.com>
2024-10-12 21:09:27 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from "./sections/Navbar.jsx";
+import Hero from "./sections/Hero.jsx";
+import About from "./sections/About.jsx";
+
+const App = () => {
+    return (
+        <main className='max-w mx-auto'>
+            <Navbar />
+            <Hero />
+            <About />
+        </main>
+    )
+}
+export default App
